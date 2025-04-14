0bc6e5b80e2f82d1b4b24747d6299e6a46e39b10
Angeline Widjaja <angel@Angelines-MacBook-Air.local>
2025-04-15 00:27:39 +0700

    add get following sleep record controllers

---
+++ app/controllers/followings_controller.rb
@@ -0,0 +1,27 @@
+# app/controllers/followings_controller.rb
+class FollowingsController < ApplicationController
+    before_action :set_user
+  
+    def sleep_records
+        one_week_ago = 1.week.ago
+
+        records = SleepRecord
+            .joins(:user)
+            .where(user_id: @user.following_ids)
+            .where.not(clock_out_at: nil)
+            .where("clock_in_at >= ?", one_week_ago)
+            .select(
+            'sleep_records.*, (TIMESTAMPDIFF(SECOND, clock_in_at, clock_out_at)) AS duration_seconds'
+            )
+            .order('duration_seconds DESC')
+
+        render json: records.as_json(methods: [:duration_seconds])
+    end
+  
+    private
+  
+    def set_user
+      @user = User.find(params[:user_id])
+    end
+  end
+  
