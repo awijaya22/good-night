65a0dfcaf485c33af2162d4223aee8cc33405d83
Angeline Widjaja <angel@Angelines-MacBook-Air.local>
2025-04-15 00:24:20 +0700

    defined global error handling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,14 @@
-class ApplicationController < ActionController::Base
-  # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
-  allow_browser versions: :modern
+class ApplicationController < ActionController::API
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
+  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity
+
+  private
+
+  def render_not_found(exception)
+    render json: { error: exception.message }, status: :not_found
+  end
+
+  def render_unprocessable_entity(exception)
+    render json: { error: exception.record.errors.full_messages.join(", ") }, status: :unprocessable_entity
+  end
 end
